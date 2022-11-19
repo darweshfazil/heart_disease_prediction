16a07e0d495a57b03d54c54106e9c63404133c5a
abiraj-rajendran <abiraj.rajendran@zoho.com>
2022-11-20 00:01:38 +0530

    remove uvicorn

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,8 @@
 FROM python:3.9
-RUN apt-get update
 RUN mkdir /app
 WORKDIR /app
-COPY ./src /app/src
+COPY /src /app/
 COPY ./requirements.txt /app
 RUN pip install -r requirements.txt
-EXPOSE 8000
-ENTRYPOINT ["python"]
-CMD ["main.py"]
+EXPOSE 8080
+CMD exec python main.py
--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ from db.db_helpers import *
 from models.fast_api_models import *
 from models.models import *
 from prediction.prediction import *
-import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 app=FastAPI()
 
@@ -141,6 +140,3 @@ def patient_my_records(token:str):
 def patient_date_records(request:TokenRequest):
     print(request.dict())
     return {"message":"suscces"}
-
-if __name__ == "__main__":
-  uvicorn.run("main:app", reload=True)
